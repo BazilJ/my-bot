bbf86e739ff97d70cb6fe978c6805e302321825d
Baziljavano <bazilrblx2.0@gmail.com>
2026-02-21 18:04:25 +0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import cfg
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='bzl ', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx:commands.Context):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx:commands.Context, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def btn(ctx:commands.Context):
+    view = discord.ui.View()
+    view.add_item(discord.ui.Button(
+        style=discord.ButtonStyle.blurple,
+        label="Click me",
+        custom_id=f"click_me:{ctx.author.id}"
+    ))
+    await ctx.send(view=view)
+
+@bot.event
+async def on_interaction(itr: discord.Interaction):
+    data = itr.data
+    if not data:
+        return
+    
+    user = itr.user
+    
+    if data["custom_id"] == f"click_me:{user.id}":
+        await itr.response.defer()
+        await itr.followup.edit_message(itr.message.id, view=None)
+        await itr.followup.send(f"hi {user.name}")
+
+@bot.command()
+async def epstein(ctx:commands.Context):
+    with open("images/epstein.jpeg", "rb") as f:
+        await ctx.send(file=discord.File(f))
+
+bot.run(cfg.token)
